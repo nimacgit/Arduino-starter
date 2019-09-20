7be10e6357e74bd9ff8dc8163031558cb206ec69
nimac <michael12sco@gmail.com>
2019-09-20 17:10:11 +0430

    add lcd to SG

--- GardenMonitor-platformio/src/main.cpp
+++ GardenMonitor-platformio/src/main.cpp
@@ -1,9 +1,8 @@
-#include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <SPI.h>
 #include <SD.h>
 #include <string.h>
-#include <LiquidCrystal.h>
+#include "LiquidCrystal.h"
 
 /*
 SD card: 5V
@@ -32,88 +31,102 @@ VSS - GND
 #define REDPIN 5
 #define GREENPIN 6
 #define BLUEPIN 7
-#define interruptPin 2
+#define INTERRUPTPIN 2
 #define RsPIN 10
 #define EnPIN 9
 #define D4PIN 8
 #define D5PIN 7
 #define D6PIN 6
 #define D7PIN 5
+#define DEBUGMOD false
+#define EJECTSTATE "EJ"
+#define ERRORSTATE "ER"
+#define OKSTATE "OK"
 
-LiquidCrystal  lcd(RsPIN, EnPIN, DPIN, D5PIN, D6PIN, D7PIN);
+LiquidCrystal  lcd(RsPIN, EnPIN, D4PIN, D5PIN, D6PIN, D7PIN);
 
 File myFile;
 DHT DHT_sens(DHT11_PIN, DHT11);
 
-int period = 5000;
-unsigned long time_old = 0;
+int savePeriod = 5000;
+int LCDPeriod = 1000;
+unsigned long saveTime = 0;
+unsigned long LCDTime = 0;
 float h, t;
 bool write2sd = false;
 int moisValue;
 int mois2Value;
 int mois3Value;
-char fileName[] = "TermoHu";
+char fileName[] = "SG";
 int lastFileNum = -1;
+bool interuptPinVal;
+char* state;
 
 void writeToFile();
 void changeSD();
+void startSD();
+void stopSD();
 void printState();
 void ejectState();
 void errorState();
 void okState();
 void writeState();
-void displayState();
+void displaySensor();
 void initSD();
 void initLCD();
 void initLED();
 void initDHT();
 void initInterrupt();
+void readSensors();
+void setPins();
 
 void setup()
 {
-  initLED();
   initDHT();
-  initInterrupt();
+  // initInterrupt();
   initLCD();
   Serial.begin(9600);
   initSD();
-  time_old = millis();
+  LCDTime = saveTime = millis();
 }
 
 void loop() {
-  h = DHT_sens.readHumidity();
-  t = DHT_sens.readTemperature();
-  moisValue = analogRead(MOIS_PIN);
-  mois2Value = analogRead(MOIS2_PIN);
-  mois3Value = analogRead(MOIS3_PIN);
-
-  if((millis() > time_old + period)){
-    time_old = millis();
+  readSensors();
+  setPins();
+  if((millis() > LCDTime + LCDPeriod)){
+    LCDTime = millis();
+    displaySensor();
     if(!write2sd){
       ejectState();
     }
-    else{
+  }
+  if((millis() > saveTime + savePeriod) && write2sd){
+      saveTime = millis();
       writeToFile();
-    }
   }
 }
 
+
+
+
+
+
+
+
+
+
+
 void openfile(){
-  int num = 0;
-  char strNum[5];
-  char strName[16] = "";
-  sprintf(strNum, "%d", num);
-  strcpy(strName, fileName);
-  strcat(strName, strNum);
+  int num = 1;
+  char strName[16];
+  // sprintf(strNum, "%d", num);
+  // strcpy(strName, fileName);
+  // strcat(strName, strNum);
+  sprintf(strName, "%s%d", fileName, num);
   while(SD.exists(strName) && (num < lastFileNum || lastFileNum == -1)){
     num++;
-    sprintf(strNum, "%d", num);
-    strcpy(strName, fileName);
-    strcat(strName, strNum);
+    sprintf(strName, "%s%d", fileName, num);
   }
-  sprintf(strNum, "%d", num);
-  strcpy(strName, fileName);
-  strcat(strName, strNum);
   lastFileNum = num;
   myFile = SD.open(strName, FILE_WRITE);
 }
@@ -122,10 +135,7 @@ void writeToFile(){
   openfile();
   if (myFile) {
     writeState();
-    
-    // printState();
-
-    myFile.println(time_old);
+    myFile.println(saveTime);
     myFile.print(t);
     myFile.print("C ");
     myFile.println(h);
@@ -139,13 +149,11 @@ void writeToFile(){
     okState();
   } else {
     errorState();
-    Serial.println("error opening file");
   }
 }
 
-void printState(){
-  Serial.println("done.");
-  Serial.println(time_old);
+void printSensor(){
+  Serial.println(saveTime);
   Serial.print("T:");
   Serial.print(t);
   Serial.print("C ");
@@ -158,47 +166,114 @@ void printState(){
   Serial.print(mois2Value);
   Serial.print(" ");
   Serial.println(mois3Value);
-  Serial.print("Writing to TermoHu.txt...");
+  Serial.println("Writing to TermoHu.txt...");
 }
 
-void displayState(){
+
+void displaySensor(){
   lcd.setCursor(0, 0);
-    lcd.print("T:");
-    lcd.print(t);
-    lcd.print("C ");
-    lcd.print("H:");
-    lcd.print(h);
-    lcd.print("%");
-    lcd.setCursor(0, 1);
-    lcd.print("M:");
-    lcd.print(moisValue);
-    lcd.print(" ");
-    lcd.print(mois2Value);
-    lcd.print(" ");
-    lcd.print(mois3Value);
+  lcd.print("");
+  lcd.print(t);
+  lcd.print("C ");
+  lcd.print(h);
+  lcd.print("%");
+  lcd.setCursor(0, 1);
+  lcd.print("M:");
+  lcd.print(moisValue);
+  lcd.print(" ");
+  lcd.print(mois2Value);
+  lcd.print(" ");
+  lcd.print(mois3Value);
 }
+
+void LCDEjectState();
+void LCDErrorState();
+void LCDOkState();
+void LCDWriteState();
+void LEDEjectState();
+void LEDErrorState();
+void LEDOkState();
+void LEDWriteState();
 
 void ejectState(){
+  state = EJECTSTATE;
+  if(DEBUGMOD){
+    Serial.println("Eject mode");
+  }
+  LCDEjectState();
+  // LEDEjectState();
+}
+void errorState(){
+  state = ERRORSTATE;
+  if(DEBUGMOD){
+    Serial.println("error opening file");
+  }
+  LCDErrorState();
+  // LEDErrorState();
+}
+void okState(){
+  state = OKSTATE;
+  if(DEBUGMOD){
+    Serial.println("Okaye");
+  }
+  LCDOkState();
+  // LEDOkState();
+}
+void writeState(){
+  if(DEBUGMOD){
+    Serial.println("writing");
+    printSensor();
+  }
+  LCDWriteState();
+  // LEDWriteState();
+}
+
+void LEDEjectState(){
   digitalWrite(REDPIN, HIGH);
   digitalWrite(GREENPIN, LOW);
   digitalWrite(BLUEPIN, LOW);
 }
 
-void errorState(){
+void LEDErrorState(){
   digitalWrite(REDPIN, LOW);
   digitalWrite(GREENPIN, LOW);
   digitalWrite(BLUEPIN, HIGH);
 }
 
-void writeState(){
+void LEDWriteState(){
   digitalWrite(REDPIN, LOW);
   digitalWrite(GREENPIN, LOW);
   digitalWrite(BLUEPIN, HIGH);
 }
-void okState(){
+void LEDOkState(){
   digitalWrite(REDPIN, LOW);
   digitalWrite(GREENPIN, HIGH);
   digitalWrite(BLUEPIN, LOW);
+}
+
+void LCDEjectState(){
+  lcd.setCursor(13,0);
+  lcd.print(" EJ");
+}
+void LCDErrorState(){
+  lcd.setCursor(13,0);
+  lcd.print(" ER");
+}
+void LCDOkState(){
+  lcd.setCursor(13,0);
+  lcd.print(" OK");
+}
+void LCDWriteState(){
+  lcd.setCursor(13,0);
+  lcd.print(" WR");
+}
+
+void startSD(){
+  write2sd = true;
+}
+
+void stopSD(){
+  write2sd = false;
 }
 
 void changeSD(){
@@ -220,20 +295,45 @@ void initDHT(){
 }
 
 void initInterrupt(){
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), changeSD, FALLING);
+  pinMode(INTERRUPTPIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPTPIN), changeSD, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(INTERRUPTPIN), stopSD, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(INTERRUPTPIN), startSD, RISING);
 }
 
 void initSD(){
-  Serial.print("Initializing SD card...");
+  if(DEBUGMOD){
+    Serial.print("Initializing SD card...");
+  }
   while(!SD.begin(4)){
-    Serial.println("initialization failed!");
+    if(DEBUGMOD){
+      Serial.println("initialization failed!");
+    }
     delay(1000);
   }
-  Serial.println("initialization done.");
+  if(DEBUGMOD){
+    Serial.println("initialization done.");
+  }
 }
 
 void initLCD(){
   lcd.begin(16, 2);
   lcd.clear();
+  lcd.print("Hello 2 SG");
+}
+
+void readSensors(){
+  h = DHT_sens.readHumidity();
+  t = DHT_sens.readTemperature();
+  moisValue = analogRead(MOIS_PIN);
+  mois2Value = analogRead(MOIS2_PIN);
+  mois3Value = analogRead(MOIS3_PIN);
+}
+
+void setPins(){
+  if(digitalRead(INTERRUPTPIN) == HIGH){
+    write2sd = true;
+  } else{
+    write2sd = false;
+  }
 }
