b8cf1760e70b57a27d6f98fbea238fad7ca5f1a7
nimac <michael12sco@yahoo.com>
2019-10-02 22:43:35 +0330

    add more shape to sound meter

--- SounderMeter/src/main.cpp
+++ SounderMeter/src/main.cpp
@@ -25,8 +25,11 @@ void clear();
 void Display(unsigned char dat[8][8]);
 void drawScreen(byte buffer2[]);
 void drawhi();
+void clearState(bool state[8][8]);
 void display(boolean state[8][8]);
-void drawCircle();
+void drawAlert();
+void drawCircle(bool state[8][8]);
+void drawCircleSquare();
 bool shouldAlert(int sound);
 
 void setup() {
@@ -43,7 +46,7 @@ void loop(){
   int sound = analogRead(A0);
   if(shouldAlert(sound)){
     for(int k = 0; k < 3; k++){
-      drawCircle();
+      drawAlert();
     }
     Serial.println(sound);
   }
@@ -63,6 +66,9 @@ struct Pair
 {
   public:
     int x, y;
+    Pair(){
+
+    }
     Pair(int x1, int y1){
       x = x1;
       y = y1;
@@ -70,24 +76,63 @@ struct Pair
 };
 
 void drawAlert(){
-  
+  // drawCircle();
+  drawCircleSquare();
 }
 
-void drawCircle(){
+void drawCircleSquare(){
   bool state[8][8];
+  clearState(state);
+  Pair squareLoc[36];
   for(int i = 0; i < 8; i++){
+    squareLoc[4*i] = Pair(0,i);
+    squareLoc[4*i+1] = Pair(i,0);
+    squareLoc[4*i+2] = Pair(i,7);
+    squareLoc[4*i+3] = Pair(7,i);
+  }
+  squareLoc[32] = Pair(3,3);
+  squareLoc[33] = Pair(3,4);
+  squareLoc[34] = Pair(4,3);
+  squareLoc[35] = Pair(4,4);
+  Pair locations[12] = {Pair(1,3), Pair(1,4), Pair(2,5), Pair(3,6), Pair(4,6), Pair(5,5), Pair(6,4),
+    Pair(6,3), Pair(5,2), Pair(4,1), Pair(3,1), Pair(2,2)};
+  for(int j = 0; j < 36; j++){
+    int x = squareLoc[j].x;
+    int y = squareLoc[j].y;
+    state[x][y] = true;
+  }
+  drawCircle(state);
+  for(int j = 0; j < 36; j++){
+    int x = squareLoc[j].x;
+    int y = squareLoc[j].y;
+    state[x][y] = false;
+  }
+}
+
+
+void drawCircle(bool state[8][8]){
+  Pair locations[12] = {Pair(1,3), Pair(1,4), Pair(2,5), Pair(3,6), Pair(4,6), Pair(5,5), Pair(6,4),
+    Pair(6,3), Pair(5,2), Pair(4,1), Pair(3,1), Pair(2,2)};
+  for(int i = 0; i < 12; i++){
+    for(int j = 0; j < 4; j++){
+      int x = locations[(i+j)%12].x;
+      int y = locations[(i+j)%12].y;
+      state[x][y] = true;
+    }
+    display(state);
+    for(int j = 0; j < 4; j++){
+      int x = locations[(i+j)%12].x;
+      int y = locations[(i+j)%12].y;
+      state[x][y] = false;
+    }
+  }
+}
+
+void clearState(bool state[8][8]){
+    for(int i = 0; i < 8; i++){
     for(int j = 0; j < 8; j++){
       state[i][j] = false;
     }
-  }
-  Pair locations[12] = {Pair(1,3), Pair(1,4), Pair(2,5), Pair(3,6), Pair(4,6), Pair(5,5), Pair(6,4),
-  Pair(6,3), Pair(5,2), Pair(4,1), Pair(3,1), Pair(2,2)};
-  for(int i = 0; i < 12; i++){
-    int x = locations[i].x;
-    int y = locations[i].y;
-    state[x][y] = true;
-    display(state);
-    state[x][y] = false;
   }
 }
 
@@ -102,15 +147,22 @@ void turnoff(int x, int y){
 }
 
 void display(bool state[8][8]){
-  for(int i = 0; i < 8; i++){
-    for(int j = 0; j < 8; j++){
-      if(state[i][j]){
-        for(int k = 0; k < 2000; k++){
-          turnon(7- j, 7 - i);
+  for(int k = 0; k < 5; k++){
+    for(int i = 0; i < 8; i++){
+      for(int j = 0; j < 8; j++){
+        for(int l = 0; l < 40; l++){
+          if(state[i][j]){
+            turnon(7- j, 7 - i);
+          }
         }
         turnoff(7- j, 7 - i);
       }
     }
+  }
+  for(int i = 0; i < 8; i++){
+      for(int j = 0; j < 8; j++){
+        turnoff(7- j, 7 - i);
+      }
   }
 }
 
