ea2f93662994000d146c6d0a99301e8161995ec2
nimac <michael12sco@yahoo.com>
2020-07-17 17:18:47 +0430

    simple change in mois water logic

--- garden-watering/src/main.cpp
+++ garden-watering/src/main.cpp
@@ -21,16 +21,21 @@ VSS - GND
 #define DRYMOISMIN 400
 #define DRYMOISMAX 600
 #define WETMOIS 300
-// #define NORMALWATERINGTRY 5
-// #define LOWWATERINGTRY 3
-#define NORMAL_WATERING_DELAY 300
-#define LOW_WATERING_DELAY 600
 #define LOWWATERMODE false
 #define RELAYPIN 8
 #define MAX_SENSORS 100
 #define BUTTON_PIN 13
 #define debug false
-
+#define MS_0 0UL
+#define MS_1MIN 60000UL
+#define MS_10MIN 600000UL
+#define MS_30MIN 1800000UL
+#define MS_1H 3600000UL
+#define MS_2H 7200000UL
+#define MS_6H 21600000UL
+#define MS_12H 43200000UL
+#define RELAY_DELAY_MS 1000UL
+#define MAX_MOIS_PROPER_VALUE 750
 
 uint8_t mois_analog_pins[] = {A0, A1};
 uint8_t mois_digital_pins[] = {9, 10};
@@ -38,15 +43,14 @@ int mois_values[MAX_SENSORS];
 int mois_state[MAX_SENSORS];
 int number_of_sensors = *(&mois_analog_pins + 1) - mois_analog_pins;
 int button_state = 0;
-unsigned long moise_watering_delay = 200;
-unsigned long auto_water_delay = 600;
+unsigned long moise_watering_delay = MS_10MIN;
+unsigned long auto_water_delay = MS_10MIN;
 bool moise_watering = false;
 bool auto_watering = false;
 unsigned long last_time_auto_watering = 0;
 unsigned long last_time_moise_watering = 0;
+unsigned long time_now = 0;
 LiquidCrystal lcd(RsPIN, EnPIN, D4PIN, D5PIN, D6PIN, D7PIN);
-int normal_watering_delay = NORMAL_WATERING_DELAY;
-int low_watering_delay = LOW_WATERING_DELAY;
 
 void setup(){
   Serial.begin(9600);
@@ -60,49 +64,61 @@ void setup(){
   // attachInterrupt(digitalPinToInterrupt(BUTTON_PI), , CHANGE);
 }
 
-
+void show_time(unsigned long t, LiquidCrystal lcd){
+  if(t >= MS_1H){
+    lcd.print((int)(t/MS_1H));
+    if(t % MS_1H > 0){
+      lcd.print(":");
+      lcd.print((t % MS_1H)/MS_1MIN);
+    }
+    lcd.print(" H ");
+  }else{
+    lcd.print(t/MS_1MIN);
+    lcd.print(" M ");
+  }
+}
 void loop() {
+  time_now = millis();
+  lcd.clear();
   button_state = digitalRead(BUTTON_PIN);
   if (button_state == HIGH) {
     switch (auto_water_delay){
-      case 600:
-      auto_water_delay = 1800;
+      case MS_10MIN:
+      auto_water_delay = MS_30MIN;
       break;
 
-      case 1800:
-      auto_water_delay = 3600;
+      case MS_30MIN:
+      auto_water_delay = MS_1H;
       break;
 
-      case 3600:
-      auto_water_delay = 7200;
+      case MS_1H:
+      auto_water_delay = MS_2H;
       break;
 
-      case 7200:
-      auto_water_delay = 21600;
+      case MS_2H:
+      auto_water_delay = MS_6H;
       break;
 
-      case 21600:
-      auto_water_delay = 43200;
+      case MS_6H:
+      auto_water_delay = MS_12H;
       break;
 
-      case 43200:
-      auto_water_delay = 600;
+      case MS_12H:
+      auto_water_delay = MS_0;
       break;
+
+      case MS_0:
+      auto_water_delay = MS_10MIN;
+      break;
+
     }
   }
+
   if(LOWWATERMODE){
-    moise_watering_delay = (unsigned long)(2 * auto_water_delay / 3);
+    moise_watering_delay = (unsigned long)(2UL * auto_water_delay / 3UL);
   }else{
-    moise_watering_delay = (unsigned long)(auto_water_delay / 3);
+    moise_watering_delay = (unsigned long)(auto_water_delay / 3UL);
   }
-
-  if(last_time_auto_watering + auto_water_delay * (unsigned long)1000 < millis()){
-    auto_watering = true;
-    last_time_auto_watering = millis();
-  }else{
-    auto_watering = false;
-  }
-
 
   for(int i = 0; i < number_of_sensors; i++){
     mois_values[i] = analogRead(mois_analog_pins[i]);
@@ -121,15 +137,23 @@ void loop() {
     if(mois_state[i] == LOW){
       is_one_wet = true;
     }
-    if(mois_state[i] == HIGH and mois_values[i] < 650){
+    if(mois_state[i] == HIGH and mois_values[i] < MAX_MOIS_PROPER_VALUE){
       is_dry = true;
     }
   }
 
+  if(auto_water_delay < time_now - last_time_auto_watering){
+    auto_watering = true;
+    last_time_auto_watering = time_now;
+  }else{
+    auto_watering = false;
+  }
   
-  if(!auto_watering && is_dry && !is_one_wet && (last_time_moise_watering + moise_watering_delay * (unsigned long)1000 < millis())){
-    moise_watering = true;
-    last_time_moise_watering = millis();
+  if(moise_watering_delay < time_now - last_time_moise_watering){
+    last_time_moise_watering = time_now;
+    if(is_dry && !is_one_wet){
+      moise_watering = true;
+    }
   }
   else{
     moise_watering = false;
@@ -148,7 +172,7 @@ void loop() {
     Serial.print("\t");
     Serial.print(last_time_moise_watering);
     Serial.print("\t");
-    Serial.print(millis());
+    Serial.print(time_now);
     Serial.print("\n");
   }
 
@@ -158,26 +182,18 @@ void loop() {
     lcd.print("-");
   }
   lcd.setCursor(0, 1);
-  if(auto_water_delay >= 3600){
-    lcd.print((int)(auto_water_delay/3600));
-    if(auto_water_delay % 3600 > 0){
-      lcd.print(":");
-      lcd.print((auto_water_delay % 3600)/60);
-    }
-    lcd.print(" H ");
-  }else{
-    lcd.print(auto_water_delay/60);
-    lcd.print(" M ");
-  }
-  lcd.print((int)((last_time_auto_watering + auto_water_delay * (unsigned long)1000 - millis())/60000));
+  show_time(auto_water_delay, lcd);
+  show_time(moise_watering_delay, lcd);
+  lcd.print((last_time_auto_watering - time_now + auto_water_delay)/MS_1MIN);
   lcd.print(" ");
-  lcd.print((int)((last_time_moise_watering + moise_watering_delay * (unsigned long)1000 - millis())/60000));
-  lcd.print(" ");
+  lcd.print((last_time_moise_watering - time_now + moise_watering_delay)/MS_1MIN);
   
   if(moise_watering || auto_watering){
     digitalWrite(RELAYPIN, LOW);
+    delay(RELAY_DELAY_MS);
+    digitalWrite(RELAYPIN, HIGH);
   }else{
-    digitalWrite(RELAYPIN,HIGH);
+    digitalWrite(RELAYPIN, HIGH);
   }
   delay(1000);
 }
